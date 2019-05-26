6363c70024801dfa4f732017be02da4b1d564f9d
mgonz893 <50932991+mgonz893@users.noreply.github.com>
2019-05-26 16:55:42 -0400

    Add files via upload

--- UDP/UDPClient.py
+++ UDP/UDPClient.py
@@ -1,14 +1,24 @@
 #UDPClient.py
 
-from socket import socket, SOCK_DGRAM, AF_INET
+from socket import socket, SOCK_DGRAM, AF_INET, timeout as TimeoutException
 
 serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = raw_input('Input lowercase sentence: ')
-clientSocket.sendto(message, (serverName, serverPort))
-modifiedMessage, addr = clientSocket.recvfrom(2048)
-print modifiedMessage, addr
+message = input('Input lowercase sentence: ')
+clientSocket.settimeout(1.0)
+# Message is sent to server
+clientSocket.sendto(message.encode(), (serverName, serverPort))
+try:
+    modifiedMessage, addr = clientSocket.recvfrom(2048)
+    print(modifiedMessage, addr)
+except TimeoutException:
+    print("Timeout!\n")
+if not message:
+# Server modifies the message to all uppercase and returns to client
+    print(modifiedMessage, addr)
+
 clientSocket.close()
 
-#Allow the client to give up if no response has been reveived within 1 second.
+#Allow the client to give up if no response has been received within 1 second.
+#Added timeout on line 9
--- UDP/UDPServer.py
+++ UDP/UDPServer.py
@@ -2,6 +2,7 @@
 
 #UDP (SOCK_DGRAM) is a datagram-based protocol. You send one 
 #datagram and get one reply and then the connection terminates.
+import random, time 
 from socket import socket, SOCK_DGRAM, AF_INET
 
 #Create a UDP socket 
@@ -10,16 +11,28 @@ from socket import socket, SOCK_DGRAM, AF_INET
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 # Assign IP address and port number to socket
 serverSocket.bind(('', 12000))
-print "Waiting for connections"
+serverSocket.settimeout(60)
+print("Waiting for connections")
+#Until a message is sent from the client, we do not go into this.
 while True:
+    # Generate a random number to use for dropping packets
+    start_time = time.time()
+    rand = random.randint(0, 10)
     # Receive the client packet along with the address it is coming from
     message, address = serverSocket.recvfrom(2048)
     # Capitalize the message from the client
-    print message, address
+    print(message, address)
     message = message.upper()
+    if rand < 6:
+        print("Dropped the packet!\n")
+        continue
+    # Send message back to client
+    timePrint = time.time() - start_time
+    print("Time: ", timePrint)
     serverSocket.sendto(message, address)
 serverSocket.close()
 
 
 #Configure the server so that it randomly drops packets.
+# see line 18, 24
 #Include information about how long each response took. This will be the RTT.
